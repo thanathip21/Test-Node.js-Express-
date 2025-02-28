35c464c7801365989b6ee0eea98cbfb2ec337354
thanathip isariyathamkul <141750177+thanathip21@users.noreply.github.com>
2025-02-28 18:41:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const debug = require('debug')('app');
 const app = express();
 const port = 3000;
 const path = require('path');
+const productRouter = express.Router();
 app.use(express.static(path.join(__dirname, "/public/")));
 
 
 app.set("views","./src/views");
 app.set("view engine", "ejs")
+
+
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World! I'm Product");
+});
+
+app.use("/products", productRouter)
+
 app.get('/', (req, res) => {
 
     res.render('index',{username: "leoza5555+", customers: ["thanthip999","Phimchanok777","Leoyuuu"]});
